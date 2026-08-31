90dfb10b82e91100738c368143e2cc20089070f9
Manuela Souza <manuela.souza5462@gmail.com>
2026-08-30 23:49:05 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/manuelasouzaa/spring/kotlin/study/SpringKotlinStudyApplication.kt
@@ -0,0 +1,11 @@
+package br.com.manuelasouzaa.spring.kotlin.study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinStudyApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/manuelasouzaa/spring/kotlin/study/SpringKotlinStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.manuelasouzaa.spring.kotlin.study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinStudyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
